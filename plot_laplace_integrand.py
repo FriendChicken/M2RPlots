918f4d2baa5fc8fa092c00ced837aff4f58abe51
Peng <pp1522@ic.ac.uk>
2024-06-07 20:46:25 +0100

    finish family of integrands

--- plot_laplace_integrand.py
+++ plot_laplace_integrand.py
@@ -2,25 +2,40 @@ from manim import *
 class GraphExample(Scene):
     def construct(self):
         axes = Axes(
-            x_range=[-3.3, 3.3],
-            y_range=[-0.5, 3,3],
-            x_axis_config={"numbers_to_include": np.arange(-3, 4, 1)},
-            y_axis_config={"numbers_to_include": np.arange(-1, 4, 1)},
+            x_range=[-0.3, 3.3],
+            y_range=[-0.5, 1.3],
+            x_axis_config={"numbers_to_include": np.arange(0, 4, 1)},
+            y_axis_config={"numbers_to_include": np.arange(-1, 2, 1)},
         )
         labels = axes.get_axis_labels(
             x_label=Tex("$t$"), y_label=Tex("$y$")
         )
         self.play(Write(axes, lag_ratio=0.01, run_time=1))
-        for i in [1,5,10,20,100,200]:
+        x_list=[1,5,25,125,625,3125]
+        all_labels = []
+        all_x = [-1,-0.7,-0.4,0,0.4,0.9]
+        all_x_val = [3,2,1.35,1.11,1.03,1]
+        all_graph = []
+        start_color = BLUE
+        end_color = YELLOW
+        for i in range(len(x_list)):
             def integrand_func(x):
-                return np.exp(i*(np.log(x)-x+1)
+                if not x:
+                    return 0
+                return np.exp(x_list[i]*(np.log(x)-x+1))
             integrand_graph = axes.plot(
             integrand_func,
-            x_range=[-4,4,0.01],
-            color=RED,
+            x_range=[0,4,0.01],
+            color=interpolate_color(start_color, end_color, i / len(x_list)),
             use_smoothing = True
             )
-            integrand_func.set_opacity(0.99-1/i)
-            label = axes.get_graph_label(integrand_graph, "x = "+ str(i))
-            self.play(Create(integrand_graph),label)
-        self.wait()
+            integrand_graph.set_stroke(opacity = 1/3 + i*(2/3) / len(x_list))
+            label = axes.get_graph_label(integrand_graph, label =MathTex("x = "+ str(x_list[i])),x_val=all_x_val[i])
+            all_labels.append(label)
+            all_graph.append(integrand_graph)
+        all_labels[0].shift(DOWN)
+        arrow = Arrow(start=[3.5, 0.5,0], end=[-1, 0.5,0], color=GRAY)
+        self.add(labels,arrow,*all_graph, *all_labels)
+        title = Text("Graph of integrands as x increases", font_size=30)
+        title.to_edge(UP)
+        self.add(title)
